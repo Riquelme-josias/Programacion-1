050722da6ff662639292de745decdbbedacf9314
Ezequiel-dew <josiasriquelme8@gmail.com>
2022-03-28 17:04:32 -0300

    subo cambios

--- ejercicio-2/variables-funciones/index.js
+++ ejercicio-2/variables-funciones/index.js
@@ -1,6 +1,32 @@
 //funcion clasica
 function mostrar(){
     
-    alert('probando')
+    //forma 1
+    // const nombres = document.querySelector("#inp_nombres").value
+    //forma 2
+    const nombres = documoent.querySelector("#inp_nombres").value;
+
+    //sin template string
+    //const mensaje = 'nombre y apellido:  ' +nombres 
+
+    //sin template string
+    const mensaje = 'nombre y apellido:  ${nombres} - DNI 48258963'
+    
+    alert(mensaje)
+
+}
+
+function mifuncion_con_retorno()
+{
+    const ejemplo = 'probando funcion con retorno'
+
+    return ejemplo
+}
+
+function mostrar_respuesta()
+{
+    //acabo de ejecutar la funcion
+    const respuesta = mifuncion_con_retorno()
+    alert(respuesta)
 
 }
